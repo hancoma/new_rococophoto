0a4f8427d49e31687af057725d3a2fc2a1b3a4f3
hancoma <hancoma@gmail.com>
2014-12-05 13:03:12 +0900

    서버에서 left_menu가져오기
    uuid를  보내서 left_menu가져오기

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,23 @@
+function left_menu() {
+    var uuid=device.uuid;
+     $( document ).ready(function() {
+                    $.post("http://m.rococophoto.net/left_menu.php",
+               {
+                   
+                        uuid:uuid
+               },
+               function(data){
+                var data=data;
+                 if (data) {
+                        $("#menu").html(data);
+                    }
+
+                    
+                      
+                    
+                });
+     }
+}
 function getpage(uuid,page) {
     // 외부 페이지 가져 오기 
         // uuid는 기기 번호 
