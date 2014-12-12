c2cd129a4208727cff5a1bc4c9ca1c8ef4c5c7a0
hancoma <hancoma@gmail.com>
2014-12-12 09:31:48 +0900

    내 사진 올리기 처리
    내 사진 올리기 처리

--- www/js/app.js
+++ www/js/app.js
@@ -18,12 +18,25 @@ function left_menu() {
                 });
      });
 }
-
+var link;
+var uuid;
 function goHref(url) {
   var url=url;
   var uuid=device.uuid;
   url="http://m.rococophoto.net"+url+"&uuid="+uuid;
   var ref = window.open(url, '_blank', 'location=no');
+  ref.addEventListener('loadstart', function(event) { 
+     navigator.notification.activityStart("Rococophoto", "loading".event.url);
+        // 링크 주소 확인
+        var link=event.url;
+        var result=link.indexOf('upload_file');
+        alert(result);
+        // 파일 업로드 
+        if(result>-1) {
+            getImage();
+        }
+  
+  });
 }
 
 function getpage(uuid,page) {
@@ -71,7 +84,7 @@ function check_uuid (deviceid) {
                 if (data=='ok') {
                     gopage("around.html?ok=ok");
                 } else {
-                  alert(data);
+                  
                 show_login();
         
                 }
@@ -122,3 +135,57 @@ function onConfirm(buttonIndex) {
      gopage("index.html?ok=ok");
 }
 
+function alert_msg(title,msg,btn) {
+    // alert 대신 사용할 함수 
+    var title=title;
+    var msg=msg;
+    var btn=btn;
+
+      navigator.notification.alert(
+                  msg,  // message
+                  alertend,         // callback
+                  title,            // title
+                  btn                  // buttonName
+              );
+
+
+}
+function alertend() {
+
+}
+
+
+function getImage_photo() {
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(uploadPhoto_photo, function(message) {
+alert('get picture failed');
+},{
+quality: 50,
+destinationType: navigator.camera.DestinationType.FILE_URI,
+sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+});}
+    function uploadPhoto_photo(imageURI) {
+        var options = new FileUploadOptions();
+        options.fileKey="file";
+        options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+        options.mimeType="image/jpeg";
+
+        var params = new Object();
+        params.value1 = "test";
+        params.value2 = "param";
+        params.link=link;
+        params.uuid=uuid;
+
+        options.params = params;
+        options.chunkedMode = false;
+
+        var ft = new FileTransfer();
+        ft.upload(imageURI, "http://m.rococophoto.net/upload_org.php", win, fail, options);
+    }
+
+    function win_photo(r) {
+        console.log("Code = " + r.responseCode);
+        console.log("Response = " + r.response);
+        console.log("Sent = " + r.bytesSent);
+        alert(r.response);
+    }
